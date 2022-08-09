5844a1829dd60d95782d4d6eecc2a85160fd8c3d
Ruslan Useinov <r.useinov23@gmail.com>
2022-08-09 17:58:23 -0400

    Heroku h=10 error

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ import mainRouter from './routes/main.js'
 dotenv.config();
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
 
 // Cross Origin Resource Sharing
 const whitelist = ['http://127.0.0.1:5500', 'https://travel-comp-app.herokuapp.com'];
