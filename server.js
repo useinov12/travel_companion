d2e8707298db7ba6f0dc317d5971e6194c5550af
Ruslan Useinov <r.useinov23@gmail.com>
2022-08-13 13:54:02 -0400

    Heroku deployment: Server Node ENV check added

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ if (process.env.NODE_ENV === "production") {
     app.get("*", function (req, res) {
       res.sendFile(path.join(__dirname, "client/build", "index.html"));
     });
-  }
+}
 
 app.use((req, res, next) => {
     const error = new Error("Not found");
