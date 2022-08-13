5f9cbd24e6fbcc4b86a260c2d44013fa96febdeb
Ruslan Useinov <r.useinov23@gmail.com>
2022-08-13 12:34:43 -0400

    Heroku deployment: Server Node ENV check added

--- server.js
+++ server.js
@@ -51,19 +51,14 @@ connectDB();
 
 app.use('/api', mainRouter);
 
-// // Step 1:
-// app.use(express.static(path.resolve(__dirname, "./client/build")));
-// // Step 2:
-// app.get("*", function (request, response) {
-//   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-// });
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../client/build/index.html"));
-    });
-}
+
+// Step 1:
+app.use(express.static(path.resolve('./client/build')));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve('./client/build/index.html'));
+});
 
 
 mongoose.connection.once('open', ()=>{
